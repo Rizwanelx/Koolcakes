fe1554c15904b822d6e2657704336df7a2cf2e34
Rizwan Khan <rizwanelx@gmail.com.com>
2019-12-26 01:22:21 +0530

    make it better

--- Order_Kool/settings.py
+++ Order_Kool/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Order_Kool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [ os.path.join(BASE_DIR, 'Oredr_Kool/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
